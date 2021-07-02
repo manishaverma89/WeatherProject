f40694de0ce33752d1975b655935c30d269ce5e5
manisha <manisha.verma23@gmail.com>
2021-07-02 19:51:51 +0530

    second commit

--- app.js
+++ app.js
@@ -22,12 +22,20 @@ const url = " https://api.openweathermap.org/data/2.5/weather?q=jagadhri&appid=2
          console.log("Temperature : " + temp );
          const weatherDescription = weatherData.weather[0].description;
          console.log("DEsciption : " + weatherDescription);
-            
+        
+         // got the icon url from "https://openweathermap.org/weather-conditions"
 
+          const weatherIcon = weatherData.weather[0].icon;
+          const imageUrl = " http://openweathermap.org/img/wn/"+ weatherIcon + "@2x.png "  
+         //   we can send multiple responses to the server with res.write() method as there can be only one res.send() in any given app method 
+         res.write("<p>The Weather is currently : " + weatherDescription + "</p>");
+        res.write("<h1>The Temperature in Jagadhri is : " + temp + " degrees celcius </h1>" );
+        res.write("<img src=" + imageUrl + ">") ;
+          res.send()
         })
     })
 
-    res.send("server is up and get started");
+    // res.send("server is up and get started");
 })
 
     
